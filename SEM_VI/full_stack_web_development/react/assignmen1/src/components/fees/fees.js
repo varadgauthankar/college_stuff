60c7482ca5e37fc40b958028f65df3a735058e16
varad gauthankar <gvarad1601@gmail.com>
2022-04-19 13:27:07 +0530

    assignment 1 complete

---
+++ SEM_VI/full_stack_web_development/react/assignmen1/src/components/fees/fees.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export class Fees extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = [
+      { name: "Varad", paid: true },
+      { name: "John", paid: false },
+      { name: "Apple", paid: true },
+      { name: "Doe", paid: false },
+      { name: "Hello", paid: true },
+      { name: "Jon", paid: true },
+    ];
+  }
+  render() {
+    return (
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Fees paid</th>
+        </tr>
+        {this.state.map((e) => (
+          <tr>
+            <td>{e.name}</td>
+            <td>
+              <input type="checkbox" checked={e.paid}></input>
+            </td>
+          </tr>
+        ))}
+      </table>
+    );
+  }
+}
